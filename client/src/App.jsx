0e70bb1549ebfa3a004c030eb9985a1720082127
loof94 <gkbiswokarma62@gmail.com>
2025-03-13 08:18:15 +0545

    commitcheck

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes} from "react-router-dom";
 import {Button} from "./components/ui/Button";
 function App() {
     return(
+// my name 
         <Routes>
             <Route path="/auth" element={<AuthPage/>}/>
         </Routes>
