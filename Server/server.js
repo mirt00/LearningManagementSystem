607cdc1f868c5ae477d6d2c436e1bda36c4175f4
amrit <bhudathokiamrit@gmail.com>
2025-03-15 13:11:54 +0545

    update server

--- Server/server.js
+++ Server/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const authRoutes = require('./routes/auth-routes/index')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 cors({
-    origin: process.env.CLIENT_URL,
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE","PUT"],
     allowedHeaders:["Content-Type", "Authorization"],
 });
@@ -20,6 +20,7 @@ app.use(express.json());
 mongoose.connect(MONGO_URI).then(()=>console.log('mongodb is connected')).catch((e)=> console.log(e));
 
 //routes configuration
+app.use('/auth', authRoutes )
 
 app.use((err,req,res,next)=>{
     console.log(err.stack);
